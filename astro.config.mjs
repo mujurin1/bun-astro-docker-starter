d36c1d74e6f3dcf10baa42e3f5516a4f637cea0e
mujurin1 <mujurin25@yahoo.co.jp>
2025-06-25 00:45:57 +0900

    SSRの設定と説明を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
 import react from '@astrojs/react';
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  // static: 基本は静的, server: 基本はSSR
+  output: "static",
   integrations: [react()],
+  // サーバーサイドレンダリング時に必要
+  // dev環境ではadapterがない場合はnodeで動作するが preview 時は必要
+  // ※ preview で動作するアダプターは node のみ
+  // 他には vercel 用のアダプターなど環境に合わせて存在する
+  adapter: node({
+    mode: "standalone",
+  }),
 });
